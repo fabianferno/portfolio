30d7012b64c439ae0b1c28be0912d04b9a0bb166
Fabian Ferno <fabianferno@gmail.com>
2021-05-02 11:55:44 +0530

    Add favicon | Update Home

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/img/logo-circle-black-teal.svg";
+import { motion } from "framer-motion";
+import Logo from "../assets/img/logo-rect-black-teal.svg";
+
+const fade1 = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: [0.6, -0.05, 0.01, 0.99],
+    },
+  },
+};
 
 export default function Layout(props) {
   return (
-    <section className="bg-dark pt-5" style={{ height: "100vh" }}>
+    <motion.div
+      variants={fade1}
+      className="bg-dark pt-5"
+      style={{ height: "100vh" }}
+    >
       <header className="d-flex justify-content-around text-uppercase align-items-center">
         <Link to="/">
           <img className="" style={{ height: 100 }} src={Logo} alt="" />
@@ -22,6 +40,6 @@ export default function Layout(props) {
         </Link>
       </header>
       <div className="container pt-5">{props.children}</div>
-    </section>
+    </motion.div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,38 @@
 import Layout from "../layouts/Layout";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <Layout>
-      <h1 className="hero">Fabian Ferno</h1>
-      <div
+      <h1 className="hero pt-5">Fabian Ferno</h1>
+      <span
         style={{ letterSpacing: "7px" }}
         className="text-secondary text-uppercase"
       >
-        Web Developer | Freelancer | Graphic Designer
-      </div>
+        <motion.button
+          className="btn btn-black"
+          whileHover={{ scale: 1.025 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Web Developer
+        </motion.button>{" "}
+        |
+        <motion.button
+          className="btn btn-black"
+          whileHover={{ scale: 1.025 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Freelancer
+        </motion.button>{" "}
+        |
+        <motion.button
+          className="btn btn-black"
+          whileHover={{ scale: 1.025 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Graphic Designer
+        </motion.button>
+      </span>
     </Layout>
   );
 }
