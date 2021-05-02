8d53859dc1d1c1be9e350c5c960b59488e75a2a1
Fabian Ferno <fabianferno@gmail.com>
2021-05-02 11:02:09 +0530

    Add Custom fonts

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import Logo from "../assets/img/logo-circle-black-teal.svg";
+
 export default function Layout(props) {
   return (
-    <section className="bg-dark " style={{ height: "100vh" }}>
-      <div className="container">{props.children}</div>
+    <section className="bg-dark pt-5" style={{ height: "100vh" }}>
+      <header className="d-flex justify-content-around text-uppercase align-items-center">
+        <Link to="/">
+          <img className="" style={{ height: 100 }} src={Logo} alt="" />
+        </Link>
+        <Link className="text-primary" to="/about">
+          About
+        </Link>
+        <Link className="text-primary" to="/projects">
+          Projects
+        </Link>
+        <Link className="text-primary" to="/services">
+          Services
+        </Link>
+        <Link className="text-primary" to="/contact">
+          Contact
+        </Link>
+      </header>
+      <div className="container pt-5">{props.children}</div>
     </section>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import Layout from "../layouts/Layout";
 export default function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1 className="hero">Fabian Ferno</h1>
+      <div className="text-secondary">
+        Web Developer | Freelancer | Graphic Designer
+      </div>
     </Layout>
   );
 }
