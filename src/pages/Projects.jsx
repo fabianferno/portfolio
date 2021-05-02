25e73865eacbf44fdd46e02e0e0044dc62ab7433
Fabian Ferno <fabianferno@gmail.com>
2021-05-02 19:40:24 +0530

    Generic

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -25,11 +25,12 @@ function ProjectsCard(props) {
       /> */}
       <div className="card-body">
         <h5 className="card-title font-weight-bold text-uppercase">
-          Super Skywalker
+          {props.title ? props.title : "Super Skywalker"}
         </h5>
         <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {props.text
+            ? props.text
+            : "Some quick example text to build on the card title and make up the bulk of the card's content."}
         </p>
       </div>
     </motion.div>
