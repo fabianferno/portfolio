56af1771045e180a5687ad4a4823cdb7a7155aa0
Fabian Ferno <fabianferno@gmail.com>
2021-05-02 11:13:28 +0530

    Update logos and font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <Layout>
       <h1 className="hero">Fabian Ferno</h1>
-      <div className="text-secondary">
+      <div
+        style={{ letterSpacing: "7px" }}
+        className="text-secondary text-uppercase"
+      >
         Web Developer | Freelancer | Graphic Designer
       </div>
     </Layout>
