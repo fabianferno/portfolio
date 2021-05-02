16fdb57d6ea7ab6c15be5c94a78270c15af82227
Fabian Ferno <fabianferno@gmail.com>
2021-05-02 09:04:05 +0530

    Route pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./pages/Login";
 import Home from "./pages/Home";
-import "./assets/css/bootstrap.css";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+import "./assets/css/style.scss";
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/home" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
     </Router>
   );
