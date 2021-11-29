188413f221c04b10e40183f2f0fc853b18d610ab
Fabian Ferno <fabianferno@gmail.com>
2021-11-29 20:27:22 +0530

    Make important

--- src/components/ProjectLoader.jsx
+++ src/components/ProjectLoader.jsx
@@ -4,16 +4,13 @@ import GithubLogo from "../assets/img/github-white.png";
 export function Loader(props) {
   return (
     <motion.div
-      whileHover={{ scale: 0.9 }}
-      className="card bg-dark text-primary m-5 p-5 d-flex align-items-center"
+      whileHover={{ scale: 1.05 }}
+      className="bg-dark text-primary container d-flex align-items-center justify-content-end text-right"
     >
-      <div className="py-4 d-flex align-items-center justify-content-center  ">
-        <div className="spinner-grow text-secondary mx-2" role="status"></div>
-        <div className="spinner-grow text-secondary mx-2" role="status"></div>
-        <img src={GithubLogo} className="mx-4" alt="" srcset="" />
-        <div className="spinner-grow text-secondary mx-2" role="status"></div>
-        <div className="spinner-grow text-secondary mx-2" role="status"></div>
-      </div>
+      <img src={GithubLogo} className="mx-4" alt="" srcset="" />
+      <div className="spinner-grow text-secondary mx-2" role="status"></div>
+      <div className="spinner-grow text-secondary mx-2" role="status"></div>
+      <div className="spinner-grow text-secondary mx-2" role="status"></div>
     </motion.div>
   );
 }
