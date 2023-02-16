d530deefff78653123000f025833873d022039c9
Fabian Ferno <fabianferno@gmail.com>
2023-02-16 12:31:17 +0530

    Add more pages

--- src/pages/scripts/index.jsx
+++ src/pages/scripts/index.jsx
@@ -46,7 +46,7 @@ export default function ArticlesIndex({ articles }) {
       </Head>
       <SimpleLayout
         title="Well, you reached Skywalker's Scripts. Be discrete. He doesn't like visitors."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        intro="This is a collection of my long-form thoughts on life and the way I have seen it along the way."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
